a348803bc0e6a4f8eaea91c8e804bc64c93795b9
Masanao Daimon <diams.rider@gmail.com>
2025-05-14 22:16:29 +0900

    [Modify]accept-languageを使うようにした

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,36 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { i18nRouter } from 'next-i18n-router';
+import Negotiator from 'negotiator';
 import { i18nConfig } from '../i18nConfig';
 
+const getNegotiatedLanguage = (
+  headers: Negotiator.Headers,
+): string | undefined => {
+  return new Negotiator({ headers }).language([...i18nConfig.locales]);
+};
+
 export function middleware(request: NextRequest): NextResponse {
-  return i18nRouter(request, i18nConfig);
+  const headers = {
+    'accept-language': request.headers.get('accept-language') ?? '',
+  };
+  const preferredLanguage = getNegotiatedLanguage(headers) || i18nConfig.defaultLocale;
+
+  const pathname = request.nextUrl.pathname;
+  const pathnameIsMissingLocale = i18nConfig.locales.every(
+    (lang) => !pathname.startsWith(`/${lang}/`) && pathname !== `/${lang}`,
+  );
+
+  if (pathnameIsMissingLocale) {
+    if (preferredLanguage !== i18nConfig.defaultLocale) {
+      return NextResponse.redirect(
+        new URL(`/${preferredLanguage}${pathname}`, request.url),
+      );
+    } else {
+      const newPathname = `/${i18nConfig.defaultLocale}${pathname}`;
+      return NextResponse.rewrite(new URL(newPathname, request.url));
+    }
+  }
+
+  return NextResponse.next();
 }
 
 // このmiddlewareを適用する範囲を指定
